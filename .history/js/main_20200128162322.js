0905878acf6754d6cfffb4a5b642432d7fc018cd
Mohammad Imran <im1971cse@gmail.com>
2020-02-02 10:52:07 +0600

    push commit

---
+++ .history/js/main_20200128162322.js
@@ -0,0 +1,54 @@
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
+    x = setInterval(function() {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
+            document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        //if (distance < 0) {
+        //  clearInterval(x);
+        //  'IT'S MY BIRTHDAY!;
+        //}
+
+    }, second)
+
+
+
+function makeTimer() {
+
+    //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
+    var endTime = new Date("29 April 2020 9:56:00 GMT+01:00");
+    endTime = (Date.parse(endTime) / 1000);
+
+    var now = new Date();
+    now = (Date.parse(now) / 1000);
+
+    var timeLeft = endTime - now;
+
+    var days = Math.floor(timeLeft / 86400);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+
+    $("#days").html(days + "<span>Days</span>");
+    $("#hours").html(hours + "<span>Hours</span>");
+    $("#minutes").html(minutes + "<span>Minutes</span>");
+    $("#seconds").html(seconds + "<span>Seconds</span>");
+
+}
+
+setInterval(function() { makeTimer(); }, 1000);
---
+++ .history/main_20200128162024.js
@@ -0,0 +1,31 @@
+let target_date = new Date().getTime() + (1000 * 3600 * 48); // set the countdown date
+let days, hours, minutes, seconds; // letiables for time units
+
+let countdown = document.getElementById("tiles"); // get tag element
+
+getCountdown();
+
+setInterval(function() { getCountdown(); }, 1000);
+
+function getCountdown() {
+
+    // find the amount of "seconds" between now and target
+    let current_date = new Date().getTime();
+    let seconds_left = (target_date - current_date) / 1000;
+
+    days = pad(parseInt(seconds_left / 86400));
+    seconds_left = seconds_left % 86400;
+
+    hours = pad(parseInt(seconds_left / 3600));
+    seconds_left = seconds_left % 3600;
+
+    minutes = pad(parseInt(seconds_left / 60));
+    seconds = pad(parseInt(seconds_left % 60));
+
+    // format countdown string + set tag value
+    window.onload = countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+}
+
+function pad(n) {
+    return (n < 10 ? '0' : '') + n;
+}
